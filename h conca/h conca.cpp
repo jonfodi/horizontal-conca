7cd88ec341aec56744c21040b66bf51bb5f43e8b
jonfodi <48962228+jonfodi@users.noreply.github.com>
2019-05-14 12:38:54 -0400

    Update h conca.cpp

--- h conca/h conca.cpp
+++ h conca/h conca.cpp
@@ -1,21 +1,92 @@
-// h conca.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// horizontal concatenation of 2 matricies
+// create 2 1-d matricies by looping through rows of 2 2-matricies (for vertical conca, use COLUMNS)
+// fill the 2-d concatenated matrix by inputting the beginnning of the 2nd 1-d matrix to the end of the 1st 1-d
 
-#include "pch.h"
-#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n"; 
+
+#include<iostream>
+#include<array>
+#include"pch.h"
+#include<iomanip>
+
+using namespace std; 
+
+const size_t row{ 4 }, col{ 3 };
+
+void conca(array<int, col>&, array<int, col>&, array<int, col + col>&);
+
+void print(array<array<int, col>, row>&);
+
+void printC(array<array<int, col + col>, row>&);
+
+int main() {
+
+	array<array<int, col>, row> M1{ 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23 };
+	array<array<int, col>, row> M2{ 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24 };
+	array<array<int, col + col>, row> Mc{};
+
+	//display matricies pre-concatenation
+
+	print(M1);
+
+	cout << endl << endl;
+
+	print(M2);
+
+	cout << endl << endl;
+	
+	// create 3 new 1-d array with size of columns
+	array<int, col>r1; 
+	array<int, col>r2;
+	array<int, col + col>rc{}; // double column size for concatenated matrix
+
+	//loop through each row of M1 and M2 
+	// concatenate them with conca function
+    
+	for (int r = 0; r < row; r++) { 
+
+		for (int i = 0; i < col; i++)
+			r1[i] = M1[r][i]; // fill r1 with the rows of m1
+
+		for (int i = 0; i < col; i++) // fill r2 with the rows of m2
+			r2[i] = M2[r][i];
+
+		conca(r1, r2, rc); // concatenate r1 and r2
+		
+		for (int j = 0; j < row + row; j++)
+			Mc[r][j] = rc[j]; // fill concatenated matrix with concatenated row
+	}
+
+	printC(Mc);
+	
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void print(array<array<int, col>, row>& a) { //print 2-d matrix
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++)
+			cout << a[i][j] << " ";
+		cout << endl;
+	}
+    
+}
+void printC(array<array<int, col + col>, row>& a) { //print 2-d concatenated matrix
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col + col; j++)
+			cout << a[i][j] << " ";
+		cout << endl;
+	}
+    
+}
+
+void conca(array<int, col>& r1, array<int, col>& r2, array<int, col + col>& rc) {
+
+	for (int i = 0; i < col; i++)
+		rc[i] = r1[i]; // add row values into 2-d conca matrix
+
+	for (int i = col; i < col + col; i++)
+		rc[i] = r2[i - col]; // add row values starting from the last input of the first loop
+	
+	// process repeats for each row of M1 and M2 to concatenate them
+	// see the outer for loop controlled by size 'row'
+}
